2f24b66ab31195fdd625132960ff255650fbd353
Olivier C <oliviercoyan@hotmail.com>
2023-05-15 13:45:36 +0200

    my updates

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,11 @@
 const jwt = require ("jsonwebtoken")
+const dotenv = require('dotenv')
+dotenv.config()
 
 module.exports = async (req, res, next) => {
     try {
         const token = req.headers["authorization"].split(" ")[1]
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, (process.env.JWT_SECRET), (err, decoded) => {
         if(err){
             return res.status(401).send({
                 message : "Auth failed",
